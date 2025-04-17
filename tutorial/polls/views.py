9eb88ea20b8374bcac30bccbcb577317c675014b
Flores-cesar <111cesar1289@gmail.com>
2025-04-16 22:04:00 -0300

    [Parte 3.2] Implementa la vista index para mostrar las últimas preguntas

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", " .join([q.question_text])
+        return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
 
